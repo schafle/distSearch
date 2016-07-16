b87e75702e67bc10ed8303f9faa2e29202e895ef
wtpoo <schafle@hawk.iit.edu>
2016-07-16 01:01:14 +0000

    Script for distributed grep evaluation

---
+++ evaluation/dist-grep.sh
@@ -0,0 +1,7 @@
+if [[ $# -ne 4 ]] ; then
+    echo 'Usage:'
+    echo '     ./dist_grep.sh path_to_filename.txt path_to_aws_key.pem "Text you want to grep for" ~/directory'
+    exit 0
+fi
+#echo parallel-ssh -i -h $1 -t 10000000 -x "-oStrictHostKeyChecking=no -i $2" -l ubuntu "grep -ilr \"$3\" ~/documents"
+time parallel-ssh -i -h $1 -t 10000000 -x "-oStrictHostKeyChecking=no -i $2" -l ubuntu "grep -ilr \"$3\" $4"
